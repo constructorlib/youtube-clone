29e3840ebf7374f8ef8f72418c4b85b0179adb03
constructorlib <constructorlib@gmail.com>
2023-12-22 04:13:42 +0900

    moving icons to separate file

---
+++ client/src/components/muiIcons.js
@@ -0,0 +1,36 @@
+// muiIcons.js
+import HomeIcon from "@mui/icons-material/Home";
+import ExploreOutlinedIcon from "@mui/icons-material/ExploreOutlined";
+import SubscriptionsOutlinedIcon from "@mui/icons-material/SubscriptionsOutlined";
+import VideoLibraryOutlinedIcon from "@mui/icons-material/VideoLibraryOutlined";
+import HistoryOutlinedIcon from "@mui/icons-material/HistoryOutlined";
+import LibraryMusicOutlinedIcon from "@mui/icons-material/LibraryMusicOutlined";
+import SportsEsportsOutlinedIcon from "@mui/icons-material/SportsEsportsOutlined";
+import SportsBasketballOutlinedIcon from "@mui/icons-material/SportsBasketballOutlined";
+import MovieOutlinedIcon from "@mui/icons-material/MovieOutlined";
+import ArticleOutlinedIcon from "@mui/icons-material/ArticleOutlined";
+import LiveTvOutlinedIcon from "@mui/icons-material/LiveTvOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import FlagOutlinedIcon from "@mui/icons-material/FlagOutlined";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+import SettingsBrightnessOutlinedIcon from "@mui/icons-material/SettingsBrightnessOutlined";
+
+export {
+  HomeIcon,
+  ExploreOutlinedIcon,
+  SubscriptionsOutlinedIcon,
+  VideoLibraryOutlinedIcon,
+  HistoryOutlinedIcon,
+  LibraryMusicOutlinedIcon,
+  SportsEsportsOutlinedIcon,
+  SportsBasketballOutlinedIcon,
+  MovieOutlinedIcon,
+  ArticleOutlinedIcon,
+  LiveTvOutlinedIcon,
+  AccountCircleOutlinedIcon,
+  SettingsOutlinedIcon,
+  FlagOutlinedIcon,
+  HelpOutlineOutlinedIcon,
+  SettingsBrightnessOutlinedIcon,
+};
