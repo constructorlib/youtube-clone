78f5df2d9622f151490bd3a81016a6b05d440857
constructorlib <constructorlib@gmail.com>
2024-01-15 23:30:54 +0900

    [Videos] page init, style init

--- client/src/pages/Videos.jsx
+++ client/src/pages/Videos.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-
+import { Container, Content, Recommendations } from "./videos";
 const Videos = () => {
   return (
-    <div>
-      <p>Videos</p>
-    </div>
+    <Container>
+      <Content>content</Content>
+      <Recommendations>Recommendations</Recommendations>
+    </Container>
   );
 };
 
---
+++ client/src/pages/videos.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  gap: 24px;
+`;
+export const Content = styled.div`
+  flex: 5;
+`;
+export const Recommendations = styled.div`
+  flex: 2;
+`;
+export const VideoWrapper = styled.div``;
+export const Title = styled.h1``;
+export const Info = styled.span``;
+export const ButtonsWrapper = styled.div``;
+export const Button = styled.div``;
