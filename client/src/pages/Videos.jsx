125436728614896552a576396c5287cfa03be569
constructorlib <constructorlib@gmail.com>
2024-01-17 15:06:08 +0900

    [videos] react-youtube Embed

--- client/src/pages/Videos.jsx
+++ client/src/pages/Videos.jsx
@@ -1,8 +1,25 @@
 import React from "react";
-import { Container, Content, Recommendations } from "./videos";
+import {
+  Container,
+  Content,
+  Recommendations,
+  VideoWrapper,
+  Title,
+} from "./videos";
 const Videos = () => {
   return (
     <Container>
+      <VideoWrapper>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=rg1RoU0HF8pBSl3P"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
+      </VideoWrapper>
       <Content>content</Content>
       <Recommendations>Recommendations</Recommendations>
     </Container>
