c10b1949bd8c4c79ad2b0fb6d2486b43b69d6a32
constructorlib <constructorlib@gmail.com>
2024-01-20 03:00:00 +0900

    [Navbar] forgot to remove default styling

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
           <Input placeholder="Search" />
           <SearchOutlinedIcon />
         </Search>
-        <Link to="/signin">
+        <Link to="/signin" style={{ textDecoration: "none", color: "inherit" }}>
           <Button>
             <AccountCircleOutlinedIcon />
             SIGN IN
