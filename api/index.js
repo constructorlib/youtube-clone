395910420ed2170e12a471b344f3b8f922bd7e68
constructorlib <constructorlib@gmail.com>
2024-02-14 07:56:02 +0900

    [api/index] timeout bug fix

--- api/index.js
+++ api/index.js
@@ -19,6 +19,7 @@ const connect = () => {
       throw err;
     });
 };
+app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/comments", commentRoutes);
