54b3b425d2d074e00cf5320ba49176f2172f5e3b
constructorlib <constructorlib@gmail.com>
2024-02-14 08:28:29 +0900

    [api | auth] password hashing with bcrypt

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
 export const signup = async (req, res) => {
-  console.log(req.body);
-
-  // try {
-  //   const newUser = new User();
-  // } catch (err) {
-  //   // todo
-  // }
+  try {
+    var salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync(req.body.password, salt);
+    const newUser = new User({ ...req.body, password: hash });
+  } catch (err) {
+    // todo
+  }
 };
