9a9718233610bc7655466f53f2d8d6c0daa6e156
constructorlib <constructorlib@gmail.com>
2024-02-16 07:47:28 +0900

    [api | auth] signin controller

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -10,6 +10,29 @@ export const signup = async (req, res, next) => {
     await newUser.save();
     res.status(200).send("User has been created");
   } catch (err) {
-    next(createError(404, "not found"));
+    next(err);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ name: req.body.name });
+    if (!user) return next(createError(404, "User not found!"));
+
+    const isCorrect = await bcrypt.compare(req.body.password, user.password);
+
+    if (!isCorrect) return next(createError(400, "Wrong Credentials!"));
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT);
+    const { password, ...others } = user._doc;
+
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(others);
+  } catch (err) {
+    next(err);
   }
 };
