d8102340b992b1d28f21a39627ed0562489b6875
constructorlib <constructorlib@gmail.com>
2024-02-16 06:44:51 +0900

    [api | auth] advanced error handling

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import { createError } from "../error.js";
 export const signup = async (req, res, next) => {
   try {
     var salt = bcrypt.genSaltSync(10);
@@ -9,6 +10,6 @@ export const signup = async (req, res, next) => {
     await newUser.save();
     res.status(200).send("User has been created");
   } catch (err) {
-    next(err);
+    next(createError(404, "not found"));
   }
 };
