1ae78f15665531dc0614f5f7ea9691896769aacf
constructorlib <constructorlib@gmail.com>
2024-02-27 09:45:22 +0900

    [api | controller] small change to signin controller

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { signup } from "../controllers/auth.js";
+import { signup, signin } from "../controllers/auth.js";
 
 const router = express.Router();
 
 // Create a user
 router.post("/signup", signup);
 // Sign In
-router.post("/signin");
+router.post("/signin", signin);
 
 // Google OAUTH
 router.post("/google");
--- client/src/components/Menu/Menu.jsx
+++ client/src/components/Menu/Menu.jsx
@@ -37,7 +37,7 @@ const Menu = ({ darkMode, setDarkMode }) => {
         <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
           <Logo>
             <Img src={LamaTube} />
-            LamaTube
+            YouTube
           </Logo>
         </Link>
         <Item>
